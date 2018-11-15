d26ff7170fc11073ec2cdd54a5ad3cf6ecc60f53
artemtkachuk <artemtkachuk@yahoo.com>
2018-11-15 05:50:54 -0800

    Testing basic response to any message

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 const http = require('http')
 const qs = require('querystring');
 
-
+const token = "657188946:AAHLFiecpDW264B9E8bd2_HAUJHFlqwixU8";
 
 const server = http.createServer(function(req, res) {
 
@@ -24,11 +24,19 @@ const server = http.createServer(function(req, res) {
 
 		req.on('end', function() {
 
-			context  = console.log(JSON.parse(body));
+			message = JSON.parse(body));
+
+			console.log(message);
 
 		});
 
-		console.log(context);
+
+		chat_id = message.chat.id;
+
+		var url = 'https://api.telegra.org/bot' + token + '/' + "sendmessage?" + "chat_id=" + chat_id + "&" + "text=Бот уже работает круглосуточно и скоро сможет выполнять все свои функции!!!";
+
+		http.get(url)
+
 	}
 
 });
