7d9dc5913ca8fb64c169ae48d00a6f30c5d00a42
artemtkachuk <artemtkachuk@yahoo.com>
2018-11-19 01:19:48 -0800

    Start from scratch

--- BitcoinRate.py
+++
@@ -1,7 +0,0 @@
-from requests import get
-
-def bitcoin_rate(currency = "USD"):			#
-	return get('https://blockchain.info/ticker').json()[currency]
-
-
-#link for direct bitcoin rate to U.S. dollar: https://blockchain.info/tobtc?currency=USD&value=1
