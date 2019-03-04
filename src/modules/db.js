9f8c105952058e055816ce0d1e889307e77dfdbf
Artem Tkachuk <artemtkachuk@yahoo.com>
2019-03-04 09:58:01 -0800

    Formatted the code

--- src/modules/db.js
+++ src/modules/db.js
@@ -29,47 +29,47 @@ var make_record = function(body, type) {
 
         var props = (type == "Responses") ? {
 
-                ok: body.ok,
+            ok: body.ok,
 
-                from_id: body.result.from.id,
+            from_id: body.result.from.id,
 
-                from_is_bot: body.result.from.is_bot,
+            from_is_bot: body.result.from.is_bot,
 
-                from_username: body.result.from.username,
+            from_username: body.result.from.username,
 
-                chat_id: body.result.chat.id,
+            chat_id: body.result.chat.id,
 
-                first_name: body.result.chat.first_name,
+            first_name: body.result.chat.first_name,
 
-                last_name: body.result.chat.last_name,
+            last_name: body.result.chat.last_name,
 
-                username: body.result.chat.username,
+            username: body.result.chat.username,
 
-                message_date: Date(body.result.date * 1000),
+            message_date: Date(body.result.date * 1000),
 
-                original_text: body.result.text
+            original_text: body.result.text
 
-            } : {
+        } : {
 
-                update_id: body.update_id,
+            update_id: body.update_id,
 
-                id: body.message.from.id,
+            id: body.message.from.id,
 
-                is_bot: body.message.from.is_bot,
+            is_bot: body.message.from.is_bot,
 
-                first_name: body.message.from.first_name,
+            first_name: body.message.from.first_name,
 
-                last_name: body.message.from.last_name,
+            last_name: body.message.from.last_name,
 
-                username: body.message.from.username,
+            username: body.message.from.username,
 
-                chat_id: body.message.chat.id,
+            chat_id: body.message.chat.id,
 
-                message_date: Date(body.message.date * 1000),
+            message_date: Date(body.message.date * 1000),
 
-                original_text: body.message.text
+            original_text: body.message.text
 
-            };
+        };
 
 
         var sql_query = "INSERT INTO " + (type == "Responses" ? "Responses": "Requests") + "(";
@@ -86,15 +86,15 @@ var make_record = function(body, type) {
 
         for (var y in props) {
 
-	  if (props[y] == undefined) {
+	        if (props[y] == undefined) {
 
-		props[y] = null;
+		        props[y] = null;
 
-          }
+	        }
 
-	   sql_query += (typeof props[y] == "string") ?'\'' + props[y] + '\',' : props[y] + ',';
+	        sql_query += (typeof props[y] == "string") ?'\'' + props[y] + '\',' : props[y] + ',';
 
-	}
+	    }
 
         sql_query = sql_query.substr(0, sql_query.length - 1) + ");";
 
