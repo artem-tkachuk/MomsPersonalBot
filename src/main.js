d67bf2a2bde774f37618f03b104cc70a80d817fb
Artem Tkachuk <artemtkachuk@yahoo.com>
2019-03-04 10:49:52 -0800

    Fixed the security issue of unauthorized POST request
    Anybody who would send the POST request to the default domain without specifying the body or pathname could make the bot crash on the server.

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ var weather = require('./modules/weather.js');
 var timer = require('./modules/timer.js');
 var otherreq = require('./modules/otherreq.js');
 var db = require('./modules/db.js');
+var token = require('../../keys/token.js')
 
 
 
@@ -22,7 +23,7 @@ var curr_update = 0;    //Initial value for server startup
 
 var server = http.createServer(function(req, res) {
 
-    if (req.method == 'POST') {
+    if (req.method == 'POST' && req.pathname == token.token) {
 
         var body = '';
 
