df91c16a66884db267d28803c530fecbea326f13
Artem Tkachuk <artemtkachuk@yahoo.com>
2019-11-27 01:58:58 -0800

    commenting most of the code to make a simple get request running

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,7 +5,7 @@ import handleRoutes from './handler';
 const token = process.env.token;
 
 const router = Router();
-//router.post(`/${token}`, handleRoutes);
+router.post(`/${token}`, handleRoutes);
 router.get('/', (req, res) => {
     res.send('Hello from MomsPersonalBot!');
 });
